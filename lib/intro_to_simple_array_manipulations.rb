c1c449d627d5390f6cdb4a424059f87fb22d22f7
Troy Domio <troyakins99@yahoo.com>
2020-05-10 20:29:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+require 'pry'
+def using_push(array,string) 
+  array.push(string)
+end
+
+def using_unshift(array, string)
+array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array,2)
+  array.pop(2)
+end
